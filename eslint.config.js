c0d42bb7cd83de3be4ec957fa2b3e696d23c2959
Jeongyun Lee <solst_ice@naver.com>
2025-07-13 01:06:44 +0900

    ESLint 제외 디렉터리 추가

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,13 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
-    ignores: ["dist", "node_modules", "eslint.config.js"],
+    ignores: [
+      "dist",
+      "node_modules",
+      "eslint.config.js",
+      "client/.react-router",
+      "server/dist",
+    ],
   },
   {
     files: ["**/*.{js,ts,tsx}"],
